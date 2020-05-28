fc5b6463e1c27728631a021b5cff1e8b20d01c2f
acsa-laa <arcanjodagmar@gmail.com>
2020-05-28 17:16:17 -0300

    atualizacao projeto dia 2

--- MiniProjeto/LojaDeRoupa/Caixa.java
+++ MiniProjeto/LojaDeRoupa/Caixa.java
@@ -1,34 +1,26 @@
 package LojaDeRoupa;
-import java.util.Scanner;
 public class Caixa {
 	private int QuantidadeVendas;
 	private double ValorNoCaixa;
 	
 	public Caixa() {
 	}
-	public void Venda(double valor, int numero){
+	public String Venda(double valor, int numero){
 	    this.ValorNoCaixa = this.ValorNoCaixa + valor;
 	    this.QuantidadeVendas++;
-	    if(numero == 0) {
-	    	Scanner s = new Scanner( System.in ); 
-	    	System.out.print("Qual a quantidade de parcela?");
-	    	int quantidadeParcela = s.nextInt();
-	    	System.out.print("Compra efetuada no crédito em ");
-	    	System.out.print(quantidadeParcela);
-	    	System.out.println("parcela(s)");
-	    	
-	    }
-	    if(numero == 1) {
-	    	System.out.print("Compra efetuada no débito");
-	    }    
+	    return ("Compra efetuada no débito");    
+	}
+	public String Venda(double valor, int numero, int parcela){
+	    this.ValorNoCaixa = this.ValorNoCaixa + valor;
+	    this.QuantidadeVendas++;
+	    return ("Compra efetuada no crédito em " + parcela);    
 	}
 	
-	public void Venda(double valorCompra, double valorPago){
+	public String Venda(double valorCompra, double valorPago){
 	    this.ValorNoCaixa = this.ValorNoCaixa + valorCompra;
 	    this.QuantidadeVendas++;
 	    double troco = valorPago - valorCompra;
-	    System.out.println("Troco:");
-	    System.out.println(troco);
+	   return ("Troco:" + troco);
 	}
 	
 	public int getQuantidadeVendas() {
